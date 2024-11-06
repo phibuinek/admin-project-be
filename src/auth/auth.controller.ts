5554fa4d723e976df536375c25ce287d44584fb9
phibuinek <phibuinek@gmail.com>
2024-11-06 14:00:59 +0700

    send email by next mailer

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,10 +16,14 @@ import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './passport/local-auth.guard';
 import { JwtAuthGuard } from './passport/jwt-auth.guard';
 import { Public } from '@/decorator/customize';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly mailerService: MailerService,
+  ) {}
 
   @Post('login')
   @Public()
@@ -35,7 +39,19 @@ export class AuthController {
   }
 
   @Post('register')
-  register(@Body() registerDto : CreateAuthDto){
+  register(@Body() registerDto: CreateAuthDto) {
     return this.authService.handleRegister(registerDto);
+  }
+
+  @Get('mail')
+  @Public()
+  testMail() {
+    this.mailerService.sendMail({
+      to: 'phibuinek@gmail.com', // list of receivers
+      subject: 'Testing Nest MailerModule ✔', // Subject line
+      text: 'welcome', // plaintext body
+      html: '<b>Hello World with Phi Bùi</b>', // HTML body content
+    });
+    return 'ok';
   }
 }
