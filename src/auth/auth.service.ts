921db1014ef0762698ef12fd287026a9d6e43297
phibuinek <phibuinek@gmail.com>
2024-12-02 14:54:29 +0700

    fix bug

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -24,8 +24,9 @@ export class AuthService {
   // }
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByEmail(username);
+    if (!user) return null;
     const isValidPassword = await comparePasswordHelper(pass, user.password);
-    if (!user || !isValidPassword) {
+    if (!isValidPassword) {
       return null;
     }
     return user;
